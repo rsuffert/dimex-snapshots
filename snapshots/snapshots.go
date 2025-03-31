c25ac915cf7d1972049811f6d63a7bc23bbb08a1
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2025-03-30 23:07:06 -0300

    chore: Move snapshots into its own package (#3)
    This pull request includes significant changes to the snapshot handling
    in the `DIMEX` package. The main changes involve refactoring the
    snapshot functionality into a separate package and improving the
    snapshot-taking process.
    
    ### Snapshot Handling Refactor:
    
    *
    [`DIMEX/DIMEX-Template.go`](diffhunk://#diff-21e10de124c5cf8ae4d7061f9e6ef652bb847a15ebfe6863812183f67d308902R24):
    Refactored the snapshot handling by importing the new `snapshots`
    package and updating the `DIMEX_Module` struct and methods to use
    `snapshots.Snapshot`.
    [[1]](diffhunk://#diff-21e10de124c5cf8ae4d7061f9e6ef652bb847a15ebfe6863812183f67d308902R24)
    [[2]](diffhunk://#diff-21e10de124c5cf8ae4d7061f9e6ef652bb847a15ebfe6863812183f67d308902L74-R76)
    [[3]](diffhunk://#diff-21e10de124c5cf8ae4d7061f9e6ef652bb847a15ebfe6863812183f67d308902L157-R163)
    [[4]](diffhunk://#diff-21e10de124c5cf8ae4d7061f9e6ef652bb847a15ebfe6863812183f67d308902R275-R295)
    [[5]](diffhunk://#diff-21e10de124c5cf8ae4d7061f9e6ef652bb847a15ebfe6863812183f67d308902R324-R349)
    *
    [`DIMEX/snapshot.go`](diffhunk://#diff-13e9179b92797157356b0ae99f3df23e68c8abbaaa6902a0041e1fa6ff1f4e50L1-L105):
    Removed the old snapshot handling code from this file.
    *
    [`snapshots/snapshots.go`](diffhunk://#diff-7445aedbbb0091d50bab67428e19d87799abdc13295ec57d756fbe731dc93bf8R1-R42):
    Added a new file to handle snapshot functionality, including the
    `Snapshot` struct and the `DumpToFile` method.
    
    ### Signal Handling Improvement:
    
    *
    [`main.go`](diffhunk://#diff-2873f79a86c0d8b3335cd7731b0ecf7dd4301eb19a82ef7a1cba7589b5252261R8-R9):
    Added signal handling to gracefully shut down the application and
    potentially verify snapshot dumps upon receiving termination signals.
    [[1]](diffhunk://#diff-2873f79a86c0d8b3335cd7731b0ecf7dd4301eb19a82ef7a1cba7589b5252261R8-R9)
    [[2]](diffhunk://#diff-2873f79a86c0d8b3335cd7731b0ecf7dd4301eb19a82ef7a1cba7589b5252261L28-R30)
    [[3]](diffhunk://#diff-2873f79a86c0d8b3335cd7731b0ecf7dd4301eb19a82ef7a1cba7589b5252261R84-R96)

---
+++ snapshots/snapshots.go
@@ -0,0 +1,42 @@
+package snapshots
+
+import (
+	"SD/PP2PLink"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Snapshot struct {
+	ID              int
+	PID             int
+	State           int
+	Waiting         []bool
+	LocalClock      int
+	ReqTs           int
+	NbrResps        int
+	InterceptedMsgs []PP2PLink.PP2PLink_Ind_Message
+
+	CollectedResps int
+}
+
+func (s *Snapshot) DumpToFile() error {
+	path := fmt.Sprintf("snapshots-pid-%d.txt", s.PID)
+
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("snapshot.dumpToFile: failed opening '%s' file: %w", path, err)
+	}
+	defer file.Close()
+
+	snapshotJson, err := json.Marshal(s)
+	if err != nil {
+		return fmt.Errorf("snapshot.dumpToFile: failed marshaling snapshot to JSON: %w", err)
+	}
+
+	if _, err = file.WriteString(string(snapshotJson) + "\n"); err != nil {
+		return fmt.Errorf("snapshot.dumpToFile: failed writing to '%s' file: %w", path, err)
+	}
+
+	return nil
+}
